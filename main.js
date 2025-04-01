556213d10ecf097de6bc71a549fbc68caa02a4db
ElijahKasler <elijahkasler24@gmail.com>
2025-04-01 10:11:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,15 +8,3 @@ chrome.webRequest.onBeforeRequest.addListener(
   { urls: ["<all_urls>"] },
   ["blocking"]
 );
-
-// To redirect instead of block, use the following code
-// chrome.webRequest.onBeforeRequest.addListener(
-//   function(details) {
-//     if (details.url.includes("securly.com")) {
-//       return { redirectUrl: "https://example.com" };
-//     }
-//   },
-//   { urls: ["<all_urls>"] },
-//   ["blocking"]
-// );
-
