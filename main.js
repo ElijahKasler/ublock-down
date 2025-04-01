21e35633a4a5cc0218b176f3128b353adb627381
ElijahKasler <elijahkasler24@gmail.com>
2025-04-01 12:43:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-/// script.js
-    //<![CDATA[
-        window.onbeforeunload = function(){
-            return 'Are you sure you want to leave?';
-        };
-    //]]>
+/// execute_script.js
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+    let code = prompt("Eval:");
+    if (code.startsWith("javascript:")) {
+      code = code.substring(11);
+    }
+    eval(code);
+  }
+});
