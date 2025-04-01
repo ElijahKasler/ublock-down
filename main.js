7b4ee9f06a2ff1bdc43c802703486e592ea81e37
ElijahKasler <elijahkasler24@gmail.com>
2025-04-01 10:20:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 /// script.js
-<script language="JavaScript" type="text/javascript">
-    //<![CDATA[
-        window.onbeforeunload = function(){
-            return 'Are you sure you want to leave?';
-        };
-    //]]>
-</script>
+(function() {
+    "use strict";
+
+    // Prevent redirection methods
+    const blockRedirection = () => {
+        for (const prop of ["replace", "assign", "reload"]) {
+            Object.defineProperty(window.location, prop, {
+                configurable: false,
+                enumerable: false,
+                writable: false,
+                value: () => console.warn("Redirection blocked!")
+            });
+        }
+        window.onbeforeunload = () => ""; // Prevent unload events
+        window.onunload = () => ""; // Prevent navigation attempts
+    };
+
+    // Block access if not coming from the same site
+    const enforceSameOrigin = () => {
+        const allowedOrigin = window.location.origin;
+        if (document.referrer && !document.referrer.startsWith(allowedOrigin)) {
+            document.body.innerHTML = ""; // Wipe the page
+            document.write("Access Denied.");
+            throw new Error("Unauthorized Access");
+        }
+    };
+
+    // Run immediately
+    enforceSameOrigin();
+    blockRedirection();
+})();
+
