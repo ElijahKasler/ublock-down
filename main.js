eee43eccde7ed60f8cce4691ea44956c2ae30ac3
ElijahKasler <elijahkasler24@gmail.com>
2025-04-01 10:30:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 /// script.js
 alert("test")
+language="JavaScript" type="text/javascript">
+    //<![CDATA[
+        window.onbeforeunload = function(){
+            return 'Are you sure you want to leave?';
+        };
+    //]]>
 (function() {
     "use strict";
 
